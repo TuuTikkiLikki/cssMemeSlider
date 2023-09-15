89806acef3d7bd371755d33775299c8ea4934742
TuuTikkiLikki <tuu.tikki.notes@gmail.com>
2023-09-15 09:20:13 +0300

    feat: add logic to js file (Fri, Sep 15, 2023 09:20:13 AM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,30 @@
+
+const carouselDesktop = Array.from(document.querySelectorAll('.pagination-dot'));
+const paginationDots = Array.from(document.querySelectorAll('.dot'));
+const memes = Array.from(document.querySelectorAll('.meme'));
+
+carouselDesktop.forEach(dotArea => {
+    dotArea.addEventListener('click', () => {
+        let ind = carouselDesktop.indexOf(dotArea);
+        alignActiveDot(ind);
+        alignMeme(ind);
+    })
+  } )
+
+  function alignActiveDot(ind) {
+    paginationDots.forEach(dots => {
+      dots.classList.remove('dot-active'); 
+    })
+    paginationDots[ind].classList.add('dot-active');
+
+  }
+
+  function alignMeme(position) {
+          memes.forEach(photo => {
+          if (photo !== memes[position]) {
+            photo.classList.add('hidden');
+         }
+        })
+        memes[position].classList.remove('hidden');
+  }
+  
